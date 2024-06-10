7a1c1a73116208adf86bbde91ff4bf10bfb1aa9e
guiRodrigues20 <116125517+guiRodrigues20@users.noreply.github.com>
2024-06-10 20:59:35 -0300

    app.py

--- calculadoraPython/app.py
+++ calculadoraPython/app.py
@@ -1,22 +1,22 @@
-# Função para adicionar dois números
+
 def adicionar(x, y):
     return x + y
 
-# Função para subtrair dois números
+
 def subtrair(x, y):
     return x - y
 
-# Função para multiplicar dois números
+
 def multiplicar(x, y):
     return x * y
 
-# Função para dividir dois números
+
 def dividir(x, y):
     if y == 0:
         return "Erro: Divisão por zero não é permitida."
     return x / y
 
-# Função principal da calculadora
+
 def calculadora():
     while True:
         # Exibe o menu de opções
@@ -27,18 +27,18 @@ def calculadora():
         print("4. Dividir")
         print("5. Sair")
 
-        # Solicita a escolha do usuário
+        
         escolha = input("Digite a opcao (1/2/3/4/5): ")
 
-        # Verifica se o usuário escolheu sair
+     
         if escolha == '5':
             print("Saindo da calculadora. Até logo!")
             break
 
-        # Verifica se a escolha é uma das operações válidas
+       
         if escolha in ['1', '2', '3', '4']:
             try:
-                # Solicita os dois números ao usuário
+                
                 num1 = float(input("Digite o primeiro número: "))
                 num2 = float(input("Digite o segundo número: "))
             except ValueError:
@@ -46,7 +46,7 @@ def calculadora():
                 print("Entrada inválida. Por favor, digite números válidos.")
                 continue
 
-            # Executa a operação escolhida e exibe o resultado
+           
             if escolha == '1':
                 print(f"Resultado: {adicionar(num1, num2)}")
             elif escolha == '2':
@@ -57,8 +57,8 @@ def calculadora():
                 resultado = dividir(num1, num2)
                 print(f"Resultado: {resultado}")
         else:
-            # Trata escolha inválida de operação
+            
             print("Opção inválida. Por favor, escolha uma opção válida.")
 
-# Executa a calculadora
+
 calculadora()
